a8a4a4ecbbba32c4a56aee910cba587a5c97c107
Azzumar Aydid <azzumaraydid@gmail.com>
2021-06-21 17:38:58 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/livewire', function () {
+    return view('livewire');
+});
+
+Route::get('/vue', function () {
+    return view('vue');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
